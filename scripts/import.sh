94b7808376fd55a5d167e5e49ea041b6c1fa1b55
kanavnarula <33712591+kanavnarula@users.noreply.github.com>
2025-02-06 06:45:27 +0530

    Update import.sh

--- scripts/import.sh
+++ scripts/import.sh
@@ -5,3 +5,8 @@ echo "Printing values 2"
 echo "Soon will print dynamic values"
 echo $DATABASE
 echo $USERNAME
+echo "--------------------------"
+echo $TOKEN
+echo $1
+echo $2
+echo "--------------------------"
